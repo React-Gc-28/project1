ca9ccaa07748db7f30342e7f9e12c2b421dc0e55
Victor <vector.code11@gmail.com>
2022-04-24 04:56:19 +0100

    Worked on part of About Page

---
+++ src/components/About/Banner.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import {TextFrill} from "./TextSpecial";
+
+const Wrapper = styled.section`
+	display:flex;
+	background-image:url(${props=>props.background});
+	background-size:cover;
+	background-repeat: no-repeat;
+	align-items:center;
+	height:100vmin;
+	padding-left:5rem;
+
+	@media screen and (max-width: 768px){
+		padding-left:1rem;
+	}
+`;
+
+const Banner = ({title, src})=>{
+	return (
+		<Wrapper background={src}>
+			<TextFrill text={title}/>
+		</Wrapper>
+
+	);
+}
+
+export default Banner;
