f8d2ec167c16c3b04342671c6454dd599ed6cf21
Enx-dev <enx.co21@gmail.com>
2022-04-24 23:17:07 +0100

    update

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Map from "../../components/Map";
 const Contact = () => {
-  return <Map />;
+  return <></>;
 };
 
 export default Contact;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <HeroImg />
-      <Map />
+      {/* <Map /> */}
     </>
   );
 };
