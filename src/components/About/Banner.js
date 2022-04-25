8275589f42a4dd5e288ebcfcccadf6f7358beda0
Enx-dev <enx.co21@gmail.com>
2022-04-25 17:23:07 +0100

    modified about page

--- src/components/About/Banner.js
+++ src/components/About/Banner.js
@@ -1,27 +1,26 @@
 import styled from "styled-components";
-import {TextFrill} from "./TextSpecial";
+import { TextFrill } from "./TextSpecial";
 
 const Wrapper = styled.section`
-	display:flex;
-	background-image:url(${props=>props.background});
-	background-size:cover;
-	background-repeat: no-repeat;
-	align-items:center;
-	height:100vmin;
-	padding-left:5rem;
+  display: flex;
+  background-image: url(${(props) => props.background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  align-items: center;
+  height: 400px;
+  padding-left: 5rem;
 
-	@media screen and (max-width: 768px){
-		padding-left:1rem;
-	}
+  @media screen and (max-width: 600px) {
+    padding-left: 1rem;
+  }
 `;
 
-const Banner = ({title, src})=>{
-	return (
-		<Wrapper background={src}>
-			<TextFrill text={title}/>
-		</Wrapper>
-
-	);
-}
+const Banner = ({ title, src }) => {
+  return (
+    <Wrapper background={src}>
+      <TextFrill text={title} />
+    </Wrapper>
+  );
+};
 
 export default Banner;
